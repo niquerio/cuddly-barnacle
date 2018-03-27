9058eb2ce2e6247314eb3245d12f3387e9696859
Monique Rio <nique.rio@gmail.com>
2018-03-26 23:12:54 -0400

    working demo

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+         test: /\.mid$/,
+         use: [
+           'file-loader'
+         ]
+       }
+    ]
+  },
+};
